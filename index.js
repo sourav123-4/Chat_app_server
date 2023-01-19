9c3114e119b9afb9ad98467821e93ed9b90aa938
Reactongraph <js.developer@ongraph.com>
2023-01-19 12:20:36 +0530

    changes in files

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
+app.get("/", (req, res) => {
+  res.send("API Running!");
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
