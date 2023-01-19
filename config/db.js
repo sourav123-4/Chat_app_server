006a0cdf61b31b139963207cbcbade1bef7d5dd3
Reactongraph <js.developer@ongraph.com>
2023-01-19 23:49:04 +0530

    changes in files

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error: ${error.message}`.red.bold);
+    console.log(`Error44: ${error.message}`.red.bold);
     process.exit();
   }
 };
