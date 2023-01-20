0cf9a549460ffc1e5ae7c39d7522e0907a4275c5
Reactongraph <js.developer@ongraph.com>
2023-01-20 11:21:50 +0530

    chnage the route name

--- index.js
+++ index.js
@@ -26,11 +26,9 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+  app.get("/", (req, res) => {
+    res.send("hii all")
+  })
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
